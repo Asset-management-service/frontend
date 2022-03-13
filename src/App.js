2c1df5beded2a5da348aeddfef46c90f42397231
Eunbi Kang <jenabill@naver.com>
2022-03-13 19:23:31 +0900

    Fix: Material UI 관련 버그 수정

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
-import logo from './logo.svg';
 import './App.scss';
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <KeyboardArrowDownRoundedIcon />
     </QueryClientProvider>
   );
 }
