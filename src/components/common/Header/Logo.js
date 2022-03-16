40ad3d56f93d80f06c13ae011c36da858004dc4f
Eunbi Kang <jenabill@naver.com>
2022-03-16 23:36:36 +0900

    Refactor: Header Component styled-components 코드 수정

--- src/components/common/Header/Logo.js
+++ src/components/common/Header/Logo.js
@@ -5,15 +5,8 @@ const StyledLogo = styled.h1`
   text-align: center;
   transition: all 0.2s ease-in-out;
   color: #fe676e;
-  ${({ scroll }) =>
-    scroll
-      ? css`
-          font-size: 38px;
-        `
-      : css`
-          font-size: 60px;
-        `}
-  h1 {
+  font-size: ${({ scroll }) => (scroll ? '38px' : '60px')};
+  a {
     text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.4);
     font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS',
       sans-serif;
