31568cf3f570cba3b25345ff45b014662eddea11
Eunbi Kang <jenabill@naver.com>
2022-03-19 15:39:06 +0900

    Refactor: 소셜 로그인 이미지 변경 및 그에 따른 코드 수정

--- src/components/Login/SocialLogin.js
+++ src/components/Login/SocialLogin.js
@@ -1,16 +1,16 @@
+import kakaoIcon from '../../images/kakao-icon.png';
+import naverIcon from '../../images/naver-icon.png';
+import googleIcon from '../../images/google-icon.png';
 import {
   GOOGLE_LOGIN_URL,
   KAKAO_LOGIN_URL,
   NAVER_LOGIN_URL,
 } from '../../constants/loginUrl';
-import kakaoIcon from '../../images/kakao-icon.png';
-import naverIcon from '../../images/naver-icon.png';
-import googleIcon from '../../images/google-icon.png';
 import styled from 'styled-components';
 
 const SocialLoginWrapper = styled.div`
   width: 100%;
-  margin-top: 0.5rem;
+  margin-top: 2rem;
   h2 {
     font-size: 23px;
     margin: 2rem 0 1rem;
@@ -32,7 +32,7 @@ const SocialLoginHeading = styled.div`
   h2 {
     position: relative;
     top: -20px;
-    font-size: 23px;
+    font-size: 27px;
     font-weight: normal;
     background-color: #fff;
     margin: 0;
@@ -44,10 +44,11 @@ const SocialLoginHeading = styled.div`
 const SocialLoginLinks = styled.ul`
   display: flex;
   justify-content: center;
+  margin: 0.8rem 0;
 `;
 
 const SocialLoginLink = styled.li`
-  margin: 0 0.8rem;
+  margin: 0 1rem;
   border-radius: 50%;
   button {
     display: block;
@@ -59,6 +60,7 @@ const SocialLoginLink = styled.li`
 
 function SocialLogin() {
   const onLogin = (href) => window.location.assign(href);
+
   return (
     <SocialLoginWrapper>
       <SocialLoginHeading>
@@ -67,17 +69,17 @@ function SocialLogin() {
       <SocialLoginLinks>
         <SocialLoginLink>
           <button onClick={() => onLogin(KAKAO_LOGIN_URL)}>
-            <img src={kakaoIcon} />
+            <img src={kakaoIcon} alt="kakao icon" />
           </button>
         </SocialLoginLink>
         <SocialLoginLink>
           <button onClick={() => onLogin(NAVER_LOGIN_URL)}>
-            <img src={naverIcon} />
+            <img src={naverIcon} alt="naver icon" />
           </button>
         </SocialLoginLink>
         <SocialLoginLink>
           <button onClick={() => onLogin(GOOGLE_LOGIN_URL)}>
-            <img src={googleIcon} />
+            <img src={googleIcon} alt="google icon" />
           </button>
         </SocialLoginLink>
       </SocialLoginLinks>
