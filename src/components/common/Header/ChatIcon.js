2645d7b334f2b0eda092e5fab2c3e06a14242815
Eunbi Kang <jenabill@naver.com>
2022-03-31 19:22:56 +0900

    Refactor: Header Component UI 수정

--- src/components/common/Header/ChatIcon.js
+++ src/components/common/Header/ChatIcon.js
@@ -1,10 +1,17 @@
 import { Link } from 'react-router-dom';
 import CommentOutlinedIcon from '@mui/icons-material/CommentOutlined';
+import styled from 'styled-components';
+
+const ChatLink = styled(Link)`
+  .ChatIcon-icon {
+    font-size: 28px;
+  }
+`;
 function ChatIcon() {
   return (
-    <Link to="/chat">
-      <CommentOutlinedIcon />
-    </Link>
+    <ChatLink to="/chat">
+      <CommentOutlinedIcon className="ChatIcon-icon" />
+    </ChatLink>
   );
 }
 
