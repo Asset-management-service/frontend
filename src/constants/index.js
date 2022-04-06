73a20ad42c007c91989b3735412eead3e67c27d7
Eunbi Kang <jenabill@naver.com>
2022-04-07 01:10:11 +0900

    Refactor: 불필요한 코드 삭제

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,14 +1,3 @@
-import { bankCategory, securityCategory } from './assetCatetory';
-import { increaseColor, decreaseColor } from './stockColor';
-import { nav } from './nav';
-import { GOOGLE_LOGIN_URL, KAKAO_LOGIN_URL } from './loginUrl';
+import { GOOGLE_LOGIN_URL, KAKAO_LOGIN_URL, NAVER_LOGIN_URL } from './loginUrl';
 
-export {
-  bankCategory,
-  securityCategory,
-  increaseColor,
-  decreaseColor,
-  nav,
-  GOOGLE_LOGIN_URL,
-  KAKAO_LOGIN_URL,
-};
+export { GOOGLE_LOGIN_URL, KAKAO_LOGIN_URL, NAVER_LOGIN_URL };
