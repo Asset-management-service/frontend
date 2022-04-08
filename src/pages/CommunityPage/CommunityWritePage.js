e32e339c49e2cc2db039afb5e37d84a886680b55
Eunbi Kang <jenabill@naver.com>
2022-04-08 18:58:06 +0900

    Feature: 게시물 작성 UI 구현

--- src/pages/CommunityPage/CommunityWritePage.js
+++ src/pages/CommunityPage/CommunityWritePage.js
@@ -1,8 +1,60 @@
 import { useSearchParams } from 'react-router-dom';
+import { Button } from '../../components/common/Button';
+import { FormInput } from '../../components/common/FormInput';
+import Editor from '../../components/common/Editor';
+import { useForm } from '../../hooks';
+import styled from 'styled-components';
+
+const StyledForm = styled.form`
+  width: 90vw;
+  margin: 5rem auto;
+  input {
+    margin-bottom: 5rem;
+  }
+  .ql-container {
+    font-size: 15px;
+    min-height: 300px;
+  }
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  button {
+    margin: 1rem;
+    font-size: 17px;
+  }
+`;
+
 function CommunityWritePage() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
+  const { form, onChange } = useForm({
+    title: '',
+    content: '',
+  });
   const id = searchParams.get('id');
-  return <main>{id ? id : 0}</main>;
+
+  if (id) {
+    return <main>{id}</main>;
+  }
+  return (
+    <main>
+      <StyledForm>
+        <FormInput
+          type="text"
+          name="title"
+          value={form.title}
+          onChange={onChange}
+          placeholder="제목을 입력하세요"
+        />
+        <Editor onChange={onChange} />
+      </StyledForm>
+      <ButtonBox>
+        <Button basiccolor="#40B2B7">{id ? '수정' : '완료'}</Button>
+        <Button basiccolor="black">취소</Button>
+      </ButtonBox>
+    </main>
+  );
 }
 
 export default CommunityWritePage;
