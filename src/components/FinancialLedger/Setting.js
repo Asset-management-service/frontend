44ec5e170f6301f2df9d0bd4ca11b59145850bb9
yujiah-github <cil05265@naver.com>
2022-04-11 23:50:23 +0900

    Refactor: 모달창 로직 변경

--- src/components/FinancialLedger/Setting.js
+++ src/components/FinancialLedger/Setting.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React, {useState} from 'react';
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import { Button } from '../common/Button';
 
 const SettingListWrapper = styled.div`
     margin: auto;
@@ -10,9 +11,7 @@ const SettingList = styled.div`
     margin: 3em;
 `;
 
-function Setting(){
-
-    function SettingListTitle(props){
+function SettingListTitle(props){
 
     const SettingListTitle = styled.div`
         font-size: 20px;
@@ -24,7 +23,7 @@ function Setting(){
     );
 }
 
-    function SettingListContent(props){
+function SettingListContent(props){
 
     const SettingListContent = styled.div`
         font-size: 17px;
@@ -37,23 +36,86 @@ function Setting(){
 
     const [isOpen, setIsOpen] = useState(false);
 
-    const onModalHandler = () => {
-    setIsOpen(!isOpen)
+    const openModalHandler = () => {
+    setIsOpen(true)
     };
+
+    const closeModalHandler = () => {
+    setIsOpen(false)
+    };
+
+
+    const StyledModal = styled.div`
+  padding: 3rem;
+  border-radius: 1rem;
+  background-color: #fff;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  position: relative;
+  h1 {
+    font-size: 30px;
+    border-bottom: 1px solid lightgray;
+    padding: 0.5rem 4rem 1rem;
+    font-weight: normal;
+    margin-bottom: 3rem;
+  }
+  .Modal-close-btn {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    svg {
+      font-size: 30px;
+    }
+  }
+`;
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 5;
+  background-color: rgba(0, 0, 0, 0.5);
+  align-items: center;
+  justify-content: center;
+`;
+
+const [money,setMoney] = useState(" ");
+
+const onMoneyHandler = (event) => { //이메일 재설정
+    setMoney(event.currentTarget.value)
+}
+
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 3rem;
+  button {
+    margin: 0 1rem;
+    font-size: 20px;
+    width: 100px;
+    color: black;
+  }
+`;
 
 
     return(
         <SettingListContent>
             {props.content}
-                <ChevronRightRoundedIcon onClick={onModalHandler} >
-                {isOpen === false ? "Open Modal" : "Opened"}
+                <ChevronRightRoundedIcon onClick={openModalHandler}>
                 </ChevronRightRoundedIcon>
             
             {isOpen ===false ? null : 
-            <div onClick={onModalHandler}>
-                <div className="close-btn" onClick={onModalHandler}>&times;</div>
-                <div className="desc">HELLO WORLD!</div>
-            </div>
+            <ModalWrapper>
+                <StyledModal>
+                <div className="Modal-close-btn" onClick={closeModalHandler}>&times;</div>
+                <h1>한달 예산 금액 설정</h1>
+                <input type="text" id="money" value={money} onChange={onMoneyHandler}></input>
+                <ButtonBox>확인</ButtonBox>
+                </StyledModal>
+            </ModalWrapper>
+            
     }
         </SettingListContent>
     );
@@ -63,6 +125,13 @@ function Setting(){
 //모달창이 열리면 값을 입력 받아야함
 //모달창 안에는 취소버튼과 확인 버튼이 있음.
 
+
+
+function Setting(){
+
+
+
+    
 
     return(
         <SettingListWrapper>
