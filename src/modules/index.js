10f35030bc6835193c3ffcafad415e38d9de907e
Eunbi Kang <jenabill@naver.com>
2022-04-12 03:57:16 +0900

    Refactor: rootReducer에 post 추가

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import login from './login';
+import post from './post';
 import { all } from 'redux-saga/effects';
 
 const rootReducer = combineReducers({
   login,
+  post,
 });
 
 export function* rootSaga() {
