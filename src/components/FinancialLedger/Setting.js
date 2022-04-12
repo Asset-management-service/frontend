cedff2e8d13e67fd62bf03c12069ed99f7ed456f
yujiah-github <cil05265@naver.com>
2022-04-12 10:16:28 +0900

    Feat: SetMontlyBudget 컴포넌트 추가

--- src/components/FinancialLedger/Setting.js
+++ src/components/FinancialLedger/Setting.js
@@ -8,6 +8,22 @@ const SettingList = styled.div`
     margin: 3em;
 `;
 
+//설정 항목 제목 컴포넌트 스타일링
+const SettingListTitleWrapper = styled.div`
+        font-size: 20px;
+        font-weight: bold;
+`;
+
+//설정 항목 내용 컴포넌트 스타일링
+const SettingListContentWrapper = styled.div`
+        font-size: 17px;
+        font-weight: normal;
+        margin: 1em;
+        display: flex;
+        justify-content: space-between;
+        width: 49%;
+    `;
+
 //모달창 스타일링
 const ModalWrapper = styled.div`
     position: fixed;
@@ -57,30 +73,15 @@ const ButtonBox = styled.div`
     }
 `;
 
-function SettingListTitle(props){
-
-    const SettingListTitle = styled.div`
-        font-size: 20px;
-        font-weight: bold;
-    `;
-
+//설정 항목 제목 설정 컴포넌트
+const SettingListTitle = (props) =>{
     return(
-        <SettingListTitle>{props.title}</SettingListTitle>
+        <SettingListTitleWrapper>{props.title}</SettingListTitleWrapper>
     );
 }
 
-//
-
-function SettingListContent(props){
-    //리스트 내용 컴포넌트 스타일링
-    const SettingListContent = styled.div`
-        font-size: 17px;
-        font-weight: normal;
-        margin: 1em;
-        display: flex;
-        justify-content: space-between;
-        width: 49%;
-    `;
+//한달 예산 금액 설정
+const SetMonthlyBudget = (props) => {
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -88,40 +89,31 @@ function SettingListContent(props){
     setIsOpen(true)
     };
 
-    const closeModalHandler = () => {
-    setIsOpen(false)
-    };
+    const [budget,setBudget] = useState(" ");
 
-const [money,setMoney] = useState(" ");
+    const onMoneyHandler = (event) => { //이메일 재설정
+    setBudget(event.currentTarget.value)
+    }
 
-const onMoneyHandler = (event) => { //이메일 재설정
-    setMoney(event.currentTarget.value)
-}
 
     return(
-        <SettingListContent>
+        <SettingListContentWrapper>
             {props.content}
-                <ChevronRightRoundedIcon onClick={openModalHandler}>
-                </ChevronRightRoundedIcon>
+            <ChevronRightRoundedIcon onClick={openModalHandler}></ChevronRightRoundedIcon>
             {isOpen ===false ? null : 
             <ModalWrapper>
                 <StyledModal>
-                <div className="Modal-close-btn" onClick={closeModalHandler}>&times;</div>
                 <h1>한달 예산 금액</h1>
-                <input type="text" id="money" value={money} onChange={onMoneyHandler}></input>
-                <ButtonBox>확인</ButtonBox>
+                <input type="text" id="money" value={budget} onChange={onMoneyHandler}></input>
+                <ButtonBox>
+                    <button>버튼</button>
+                </ButtonBox>
                 </StyledModal>
             </ModalWrapper>
-            
-    }
-        </SettingListContent>
+            }
+        </SettingListContentWrapper>
     );
 }
-
-//체브론 아이콘을 누르면 모달창 열림
-//모달창이 열리면 값을 입력 받아야함
-//모달창 안에는 취소버튼과 확인 버튼이 있음.
-
 
 
 function Setting(){
@@ -135,21 +127,7 @@ function Setting(){
             <SettingList>
                 <SettingListTitle title='예산 설정'></SettingListTitle>
                 <hr align='left' width='50%'></hr>
-                <SettingListContent content='한달 예산 금액'></SettingListContent>
-            </SettingList>
-            <SettingList>
-                <SettingListTitle title='카테고리 설정'></SettingListTitle>
-                <hr align='left' width='50%'></hr>
-                <SettingListContent content='고정비 카테고리'></SettingListContent>
-                <SettingListContent content='변동비 카테고리'></SettingListContent>
-                <SettingListContent content='수익 카테고리'></SettingListContent>
-                <SettingListContent content='결제 카테고리'></SettingListContent>
-            </SettingList>
-            <SettingList>
-                <SettingListTitle title='지출 비율 설정'></SettingListTitle>
-                <hr align='left' width='50%'></hr>
-                <SettingListContent content='고정비'></SettingListContent>
-                <SettingListContent content='변동비'></SettingListContent>
+                <SetMonthlyBudget content='한달 예산 금액'></SetMonthlyBudget>
             </SettingList>
         </div>
     );
