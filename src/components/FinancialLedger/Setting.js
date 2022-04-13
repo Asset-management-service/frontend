4b8dede2bd15303e7be9e2f7f8c9240d32f417ea
yujiah-github <cil05265@naver.com>
2022-04-13 10:03:44 +0900

    Feat Modal Component Import 추가

--- src/components/FinancialLedger/Setting.js
+++ src/components/FinancialLedger/Setting.js
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 import React, {useState} from 'react';
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
 import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined';
-import { Button } from '../common/Button';
-import { flexbox, letterSpacing } from '@mui/system';
+import { ModalWrapper, StyledModal, ButtonBox} from '../common/Modal';
 
 //리스트 컴포넌트 스타일링
 const SettingList = styled.div`
@@ -28,81 +27,13 @@ const SettingListContentWrapper = styled.div`
         justify-content: space-between;
         width: 49%;
     `;
-
-//모달창 스타일링
-const ModalWrapper = styled.div`
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    z-index: 5;
-    background-color: rgba(0, 0, 0, 0.5);
-    align-items: center;
-    justify-content: center;
-    display: flex;
-`;
-
-const StyledModal = styled.div`
-    padding: 3rem;
-    border-radius: 1rem;
-    background-color: #fff;
-    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
-    text-align: center;
-    position: relative;
-    h1 {
-        font-size: 30px;
-        border-bottom: 1px solid lightgray;
-        padding: 0.5rem 4rem 1rem;
-        font-weight: normal;
-        margin-bottom: 3rem;
-    }
-    .Modal-close-btn {
-        position: absolute;
-        top: 10px;
-        right: 10px;
-        svg {
-        font-size: 50px;
-        }
-    }
-`;
-
-const ButtonBox = styled.div`
-    display: flex;
-    justify-content: center;
-    margin-top: 3rem;
-
-    .checkButton {
-        margin: 0 1rem;
-        width: 100px;
-        padding: 8px;
-        color: #1E88E5;
-        background-color: rgb(30,136,229, 0.2);
-        border: none;
-        font-size: 17px;
-        font-weight: bold;
-        border-radius: 8px;
-    }
-
-    .cancelButton {
-        margin: 0 1rem;
-        font-size: 17px;
-        width: 100px;
-        padding: 8px;
-        color: #FF5858;
-        background-color: rgb(255,88, 88 ,0.2);
-        border: none;
-        font-weight: bold;
-        border-radius: 8px;
-    }
-`;
-
 //예산 설정 입력 박스
 const InputBox = styled.input`
-    border: 2px solid lightgray;
-    border-radius: 3px;
+    border: 2px solid black;
+    border-radius: 5px;
     justify-content: center;
     font-size: 20px;
+    padding: 3px;
     margin: 1em;
 `;
 
