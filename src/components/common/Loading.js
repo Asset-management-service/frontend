24a891b578cd0be6b88e21682b9dfb1a2876e935
Eunbi Kang <jenabill@naver.com>
2022-04-18 02:09:28 +0900

    Design: Loading 컴포넌트 디자인 수정

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
 const SpinnerWrapper = styled.div`
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
