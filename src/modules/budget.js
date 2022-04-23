2cd5bb5b172a5b86834c3f3cf8607596220587cb
yujiah-github <cil05265@naver.com>
2022-04-23 23:27:30 +0900

    Refactor: 한달 예산 설정 컴포넌트에 Redux 적용

---
+++ src/modules/budget.js
@@ -0,0 +1,27 @@
+//예산 설정 상태 관리
+const SETMONTHLY_INPUT = 'budget/SETMONTHLY_INPUT';
+
+//예산 설정 액션 생성 함수 만들기
+export const setmonthlyInput = () => ({
+     type:SETMONTHLY_INPUT,
+     input,
+    });
+
+const initialState = {
+    input: '',
+};
+
+//리듀서 함수 생성
+function budget(state = initialState, action) {
+  switch (action.type) {
+      case SETMONTHLY_INPUT:
+          return {
+              ...state,
+              input:action.input,
+            };
+    default:
+      return state;
+  }
+}
+
+export default budget;
