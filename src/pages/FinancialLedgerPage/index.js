b1bad7450e7344c098804698aed811a610126bd9
yujiah-github <cil05265@naver.com>
2022-04-23 09:01:08 +0900

    Refactor: 가계부 Setting 페이지 생성

---
+++ src/pages/FinancialLedgerPage/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import SetMonthlyBudget from '../../components/FinancialLedger/SetMonthlyBudget';
+import SetCategory from '../../components/FinancialLedger/SetCategory';
+import SetExpenseRatio from '../../components/FinancialLedger/SetExpenseRatio';
+import { SettingList } from '../../components/FinancialLedger/StyledComponentInSetting';
+import SettingListTitle from '../../components/FinancialLedger/SettingListTitle';
+
+function Setting(){
+    return(
+        <div>
+            <SettingList>
+                <SettingListTitle title='예산 설정'></SettingListTitle>
+                <hr align='left' width='50%'></hr>
+                <SetMonthlyBudget content='한달 예산 금액'></SetMonthlyBudget>
+            </SettingList>
+            <SettingList>
+                <SettingListTitle title='카테고리 설정'></SettingListTitle>
+                <hr align='left' width='50%'></hr>
+                <SetCategory content='고정비 카테고리'></SetCategory>
+                <SetCategory content='변동비 카테고리'></SetCategory>
+                <SetCategory content='수익'></SetCategory>
+                <SetCategory content='결제 카테고리'></SetCategory>
+            </SettingList>
+            <SettingList>
+                <SettingListTitle title='지출 비율 설정'></SettingListTitle>
+                <hr align='left' width='50%'></hr>
+                <SetExpenseRatio content='고정비'></SetExpenseRatio>
+                <SetExpenseRatio content='변동비'></SetExpenseRatio>
+            </SettingList>
+        </div>
+    );
+}
+
+export default Setting;
