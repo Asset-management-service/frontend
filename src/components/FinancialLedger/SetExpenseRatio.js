fbcaab86a057ab42dbce0e44e93eb6d243768699
yujiah-github <cil05265@naver.com>
2022-04-23 09:00:05 +0900

    Refactor: 가계부 Setting 페이지 이동

--- src/components/FinancialLedger/SetExpenseRatio.js
+++ src/components/FinancialLedger/SetExpenseRatio.js
@@ -47,20 +47,23 @@ function SetExpenseRatio({content}){
 
     const onRatioSubmit = (event) => {
         event.preventDefault();
+        fixRatio = parseInt(fixRatio);
+        changeRatio = parseInt(changeRatio);
         let ratioSum = fixRatio + changeRatio;
+        ratioSum = parseInt(ratioSum);
         let check = /^[0-9]+$/;
         if(fixRatio == " " && !check.test(fixRatio) ||  changeRatio == " " && !check.test(changeRatio)){
             document.getElementById('setRatio').innerHTML='<b>입력 형식이 올바르지 않습니다.<b>';
             document.getElementById('setRatio').style.color='red';
         }
 
-        else if((isNaN(fixRatio) === false && isNaN(changeRatio) === false) && (ratioSum === 100)){
+        else if((isNaN(fixRatio) === false && isNaN(changeRatio) === false )&& (ratioSum === 100)){
             document.getElementById('showFixRatio').innerHTML=fixRatio;
             document.getElementById('showChangeRatio').innerHTML=changeRatio;
             setIsOpen(false)
         }
 
-        else if((isNaN(fixRatio) === false && isNaN(changeRatio) === false) && (ratioSum !== 100)){
+        else if((isNaN(fixRatio) === false && isNaN(changeRatio) === false )&& (ratioSum !== 100)){
             document.getElementById('setRatio').innerHTML='<b>총합이 100%가 아닙니다! 다시 입력해주세요!<b>';
             document.getElementById('setRatio').style.color='red';
         }
@@ -69,7 +72,7 @@ function SetExpenseRatio({content}){
     return(
         <SettingListContentWrapper>
                 <ContentPropsWrapper onClick={openRatioModalHandler}>{content}</ContentPropsWrapper>
-                <span id='showFixRatio'>--</span>
+                <span id={(content == '고정비') ? 'showFixRatio' : 'showChangeRatio'}>--</span>
             <ModalWrapper show={isOpen}>
                 <StyledModal>
                 <h1>지출 비율 설정</h1>
