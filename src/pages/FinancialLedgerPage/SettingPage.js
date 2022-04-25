cbdd7d7ad31dc09c455ff37f7dc6931ff2e6b8e5
yujiah-github <cil05265@naver.com>
2022-04-25 22:06:26 +0900

    Refactor: 깃허브 conflicts 해결

--- src/pages/FinancialLedgerPage/SettingPage.js
+++ src/pages/FinancialLedgerPage/SettingPage.js
@@ -5,33 +5,31 @@ import SetExpenseRatio from '../../components/FinancialLedger/SetExpenseRatio';
 import { SettingList } from '../../components/FinancialLedger/StyledComponentInSetting';
 import SettingListTitle from '../../components/FinancialLedger/SettingListTitle';
 
-
 function SettingPage(){
     return(
-        <div>
-            <BudgetContainer>
-            <SettingList>
-                <SettingListTitle title='예산 설정'></SettingListTitle>
-                <hr align='left' width='50%'></hr>
-                <SetMonthlyBudget content='한달 예산 금액'></SetMonthlyBudget>
-            </SettingList>
-            </BudgetContainer>
-            <SettingList>
-                <SettingListTitle title='카테고리 설정'></SettingListTitle>
-                <hr align='left' width='50%'></hr>
-                <SetCategory content='고정비 카테고리'></SetCategory>
-                <SetCategory content='변동비 카테고리'></SetCategory>
-                <SetCategory content='수익'></SetCategory>
-                <SetCategory content='결제 카테고리'></SetCategory>
-            </SettingList>
-            <SettingList>
-                <SettingListTitle title='지출 비율 설정'></SettingListTitle>
-                <hr align='left' width='50%'></hr>
-                <SetExpenseRatio content='고정비'></SetExpenseRatio>
-                <SetExpenseRatio content='변동비'></SetExpenseRatio>
-            </SettingList>
-        </div>
-    );
+    <section>
+      <SettingList>
+        <SettingListTitle title="예산 설정"></SettingListTitle>
+        <hr align="left" width="50%"></hr>
+        <SetMonthlyBudget content="한달 예산 금액"></SetMonthlyBudget>
+      </SettingList>
+      <SettingList>
+        <SettingListTitle title="카테고리 설정"></SettingListTitle>
+        <hr align="left" width="50%"></hr>
+        <SetCategory content="고정비 카테고리"></SetCategory>
+        <SetCategory content="변동비 카테고리"></SetCategory>
+        <SetCategory content="수익"></SetCategory>
+        <SetCategory content="결제 카테고리"></SetCategory>
+      </SettingList>
+      <SettingList>
+        <SettingListTitle title="지출 비율 설정"></SettingListTitle>
+        <hr align="left" width="50%"></hr>
+        <SetExpenseRatio content="고정비"></SetExpenseRatio>
+        <SetExpenseRatio content="변동비"></SetExpenseRatio>
+      </SettingList>
+    </section>
+  );
 }
+
 
 export default SettingPage;
